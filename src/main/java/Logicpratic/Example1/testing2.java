2fee3d9da8607d7faf09a5f75830b90763d4775d
chandrasekhar001 <ravichandrajyothi@gmail.com>
2024-09-23 20:43:00 +0530

    line push

--- src/main/java/Logicpratic/Example1/testing2.java
+++ src/main/java/Logicpratic/Example1/testing2.java
@@ -4,8 +4,8 @@ public class testing2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println( "testing2! pulling again" );
-
+		System.out.println( "testing2! pulling again" );
+		System.out.println( "line commit" );
 	}
 
 }
